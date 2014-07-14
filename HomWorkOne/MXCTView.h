ed374e91ef6faa5ad7f56affc38210d2ff4dbe3f
lonfee88 <lonfee88@gmail.com>
2014-07-14 10:22:17 +0800

    h

---
+++ HomWorkOne/MXCTView.h
@@ -0,0 +1,14 @@
+//
+//  MXCTView.h
+//  HomWorkOne
+//
+//  Created by longfei on 14-7-14.
+//  Copyright (c) 2014年 longfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreText/CoreText.h>
+
+@interface MXCTView : UIView
+
+@end
